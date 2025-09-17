c06d1a2aaeec21a0ee787a80f02730d8988cfaa2
Ballast <163994830+Ballastt@users.noreply.github.com>
2025-09-18 00:41:16 +0200

    issues with NgFor, therefore I imported CommonModule to use ngFor cause the import of the lib was faulty or so; also code in class added as in book instructed

--- start/apps/chapter01/cc-setters/src/app/components/vc-logs/vc-logs.component.ts
+++ start/apps/chapter01/cc-setters/src/app/components/vc-logs/vc-logs.component.ts
@@ -1,10 +1,31 @@
 import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-vc-logs',
+  standalone: true,
   templateUrl: './vc-logs.component.html',
   styleUrls: ['./vc-logs.component.scss'],
+  imports: [CommonModule]
+ 
 })
 export class VcLogsComponent {
-  @Input() vName = '';
+  private _vName: string = '';
+
+  @Input()
+  set vName(name: string) {
+    if (!name) return;
+    if (!this._vName){
+      this.logs.push(`initial version is ${name.trim()}`)
+    } else {
+      this.logs.push(`version changed to ${name.trim()}`)
+    }
+    this._vName = name;
+  }
+  get vName(): string {
+    return this._vName;
+  }
+
+  logs: string[] = [];
+ 
 }
